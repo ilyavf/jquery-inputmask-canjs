93ad8c9ada26614109cb63edcff4821873ae7315
Ilya Fadeev <fadeev.ilya@gmail.com>
2016-10-22 22:43:47 -0400

    0.3.5

---
+++ dist/cjs/jquery-inputmask-canjs.js
@@ -0,0 +1,7 @@
+/*jquery-inputmask-canjs@0.3.4#jquery-inputmask-canjs*/
+var can = require('can');
+var Inputmask = require('jquery.inputmask/dist/inputmask/inputmask');
+can.view.attr('inputmask', function (el) {
+    var config = el.getAttribute('inputmask'), im = new Inputmask(config);
+    im.mask(el);
+});
